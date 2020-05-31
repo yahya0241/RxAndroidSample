9b2e3e9846f1467aeed305755278387f1773db5e
mh <yahya.mh2299@gmail.com>
2020-05-31 14:02:07 +0430

    add concat map operation, fix bugs.

--- app/src/main/java/com/example/rxandroidsample/ui/MainActivity.kt
+++ app/src/main/java/com/example/rxandroidsample/ui/MainActivity.kt
@@ -1,32 +1,25 @@
 package com.example.rxandroidsample.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
-import com.example.rxandroidsample.DataSource
 import com.example.rxandroidsample.R
-import com.example.rxandroidsample.model.Task
 import com.google.android.material.navigation.NavigationView
-import io.reactivex.*
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
-import org.reactivestreams.Subscription
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-    private val TAG = "TAG"
-    val disposable = CompositeDisposable()
+
+    private val disposable = CompositeDisposable()
     private lateinit var navigationView: NavigationView
     private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,13 +27,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         navigationView = findViewById(R.id.nav_view)
 
         initNavigationView()
-
-//        createFlowable()
-//        createSingleObservable()
-//        justObservableTest()
-//        rangeRepeatObservableTest()
-//        intervalObservableTest()
-//        timerObservableTest()
     }
 
     private fun initNavigationView() {
@@ -52,17 +38,18 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
         when (menuItem.itemId) {
-            R.id.nav_flatMap -> {
+            R.id.nav_filter -> {
                 val navOptions = NavOptions.Builder().setPopUpTo(R.id.main, true).build()
                 Navigation.findNavController(this, R.id.nav_host_fragment)
-                    .navigate(R.id.flatScreen, null, navOptions)
+                    .navigate(R.id.filterScreen, null, navOptions)
             }
-            R.id.nav_switchMap -> {
-                if (isValidDestination(R.id.nav_switchMap)) {
+            R.id.nav_observable -> {
+                if (isValidDestination(R.id.nav_observable)) {
                     Navigation.findNavController(this, R.id.nav_host_fragment)
-                        .navigate(R.id.switchScreen)
+                        .navigate(R.id.observableScreen)
                 }
             }
+
             R.id.nav_search -> {
                 if (isValidDestination(R.id.nav_search)) {
                     Navigation.findNavController(this, R.id.nav_host_fragment)
@@ -81,19 +68,31 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                         .navigate(R.id.buffersScreen)
                 }
             }
-            R.id.nav_filter -> {
-                if (isValidDestination(R.id.nav_filter)) {
+
+
+            R.id.nav_flatMap -> {
+                if (isValidDestination(R.id.nav_flatMap)) {
                     Navigation.findNavController(this, R.id.nav_host_fragment)
-                        .navigate(R.id.filterScreen)
+                        .navigate(R.id.flatScreen)
                 }
             }
-            R.id.nav_observable->{
-                if(isValidDestination(R.id.nav_observable)){
+            R.id.nav_switchMap -> {
+                if (isValidDestination(R.id.nav_switchMap)) {
                     Navigation.findNavController(this, R.id.nav_host_fragment)
-                        .navigate(R.id.observableScreen)
+                        .navigate(R.id.switchScreen)
                 }
+            }
+            R.id.nav_concatMap -> {
+                if (isValidDestination(R.id.nav_concatMap)) {
+                    Navigation.findNavController(this, R.id.nav_host_fragment)
+                        .navigate(R.id.concatMapScreen)
+                }
+            }
+            R.id.nav_continue -> {
+                Toast.makeText(this, "New operator will be add :))", Toast.LENGTH_SHORT).show()
             }
         }
+
         menuItem.isChecked = true
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
@@ -135,180 +134,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-
-
-    private fun timerObservableTest() {
-        //The Timer operator creates an Observable that emits one particular item after a span of time that you specify.
-        Observable
-            .timer(3, TimeUnit.SECONDS)
-            .repeat(2)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
-            .subscribe(object : Observer<Long> {
-                var time: Long = 0 // variable for demonstating how much time has passed
-
-                override fun onSubscribe(d: Disposable) {
-                    Log.d(TAG, "onSubscribe: called")
-                    time = System.currentTimeMillis() / 1000
-                }
-
-                override fun onNext(i: Long) {
-                    Log.d(
-                        TAG,
-                        "onNext: " + ((System.currentTimeMillis() / 1000) - time) + " seconds have elapsed."
-                    )
-                }
-
-                override fun onError(e: Throwable) {
-                    Log.e(TAG, "onError: ", e)
-                }
-
-                override fun onComplete() {
-                    Log.d(TAG, "onComplete: done...")
-                }
-            })
-    }
-
-    private fun intervalObservableTest() {
-        // The Interval operator returns an Observable that emits an infinite sequence of ascending integers,
-        // with a constant interval of time of your choosing between emissions.
-        Observable
-            .interval(5, TimeUnit.SECONDS)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .takeWhile { aLong -> aLong <= 5 }
-            .subscribe(object : Observer<Long> {
-                override fun onSubscribe(d: Disposable) {
-                    Log.d(TAG, "onSubscribe: called")
-                }
-
-                override fun onNext(i: Long) {
-                    Log.d(TAG, "onNext: $i")
-                }
-
-                override fun onError(e: Throwable) {
-                    Log.e(TAG, "onError: ", e)
-                }
-
-                override fun onComplete() {
-                    Log.d(TAG, "onComplete: done...")
-                }
-            })
-    }
-
-    private fun rangeRepeatObservableTest() {
-        Observable.range(0, 3)
-            .repeat(2) //tow time repeat 0 to 3
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : Observer<Int> {
-                override fun onSubscribe(d: Disposable) {
-                    Log.d(TAG, "onSubscribe: called")
-                }
-
-                override fun onNext(i: Int) {
-                    Log.d(TAG, "onNext: $i")
-                }
-
-                override fun onError(e: Throwable) {
-                    Log.e(TAG, "onError: ", e)
-                }
-
-                override fun onComplete() {
-                    Log.d(TAG, "onComplete: done...")
-                }
-            })
-
-    }
-
-    private fun justObservableTest() {
-        // just can get list with at most 10 member.
-        Observable.just("first", "second", "third")
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : Observer<String> {
-                override fun onSubscribe(d: Disposable) {
-                    Log.d(TAG, "onSubscribe: called")
-                }
-
-                override fun onNext(s: String) {
-                    Log.d(TAG, "onNext: $s")
-                }
-
-                override fun onError(e: Throwable) {
-                    Log.e(TAG, "onError: ", e)
-                }
-
-                override fun onComplete() {
-                    Log.d(TAG, "onComplete: done...")
-                }
-            })
-    }
-
-    private fun createSingleObservable() {
-
-        val singleTaskObservable = Observable.create(
-            ObservableOnSubscribe<Task> { emitter ->
-                // Inside the subscribe method iterate through the list of tasks and call onNext(task)
-                //list can have at most ten member
-                 for (task in DataSource().getTaskList()) {
-                     if (!emitter.isDisposed) {
-                         emitter.onNext(task)
-                     }
-                 }
-
-                /*if (!emitter.isDisposed) {
-                    emitter.onNext(task)
-                    emitter.onComplete()
-                }*/
-            }
-        )
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-
-        singleTaskObservable.subscribe(object : Observer<Task> {
-            override fun onComplete() {
-            }
-
-            override fun onSubscribe(d: Disposable) {
-            }
-
-            override fun onNext(t: Task) {
-                Log.d(TAG, "onNext: single task: " + t.description)
-            }
-
-            override fun onError(e: Throwable) {
-            }
-        })
-    }
-
-    private fun createFlowable() {
-        val flowable = Flowable.range(0, 100)
-            .onBackpressureBuffer()
-            .subscribeOn(Schedulers.computation())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(object : FlowableSubscriber<Int?> {
-
-                override fun onComplete() {
-                }
-
-                override fun onNext(t: Int?) {
-                    Log.d(TAG, "onNext: $t")
-                }
-
-                override fun onError(t: Throwable?) {
-                    Log.e(TAG, "onError: ", t)
-                }
-
-                override fun onSubscribe(s: Subscription) {
-                }
-            })
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         disposable.clear()
     }
-
-
 }
